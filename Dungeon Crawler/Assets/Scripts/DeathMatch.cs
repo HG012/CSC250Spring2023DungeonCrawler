d668ff079b918af36ef84e4eb350fcecc0f715f1
HG012 <holter.gette@cuw.edu>
2023-05-12 01:40:52 -0500

    First Commit for HW15

--- Dungeon Crawler/Assets/Scripts/DeathMatch.cs
+++ Dungeon Crawler/Assets/Scripts/DeathMatch.cs
@@ -8,69 +8,66 @@ public class DeathMatch :
     private Inhabitant dude2;
     private GameObject dude1GO;
     private GameObject dude2GO;
+    private Rigidbody currRigidBodyOfAttacker;
+    private float attackMoveDistance = 2.5f;
+    private Vector3 attackerOriginalPosition;
+    private Inhabitant currentAttacker; // instance of inhabitant that is the current attacker.  Thus we can ask how much damage it can do and how much it can take and visually attack
+    private GameObject currentAttackerGO; //game object that represents the dude
+    private MonoBehaviour refereeInstance;
 
-    public DeathMatch(Inhabitant dude1, Inhabitant dude2, GameObject dude1GO, GameObject dude2GO)
+    public DeathMatch(Inhabitant dude1, Inhabitant dude2, GameObject dude1GO, GameObject dude2GO, MonoBehaviour refereeInstance)
     {
-        this.dudde1 = dude1;
+        this.dude1 = dude1;
         this.dude2 = dude2;
         this.dude1GO = dude1GO;
         this.dude2GO = dude2GO;
-    }
+        this.currentAttacker = this.dude1;
+        this.currentAttackerGO = this.dude1GO;
+        this.refereeInstance = refereeInstance;
+        
+}
 
-    public void D20Roll()
-    {
-        Random.Range(1, 21);
+    IEnumerator MoveObjectRoutine()
+    { 
+
+        yield return new WaitForSeconds(0.5f);
+        Vector3 originalPosition = this.attackerOriginalPosition;
+        Vector3 targetPosition = originalPosition + this.currentAttacker.transform.right * attackMoveDistance;
+
+        this.currRigidBodyOfAttacker.MovePosition(targetPosition);
+
+        yield return new WaitForSeconds(0.5f);
+
+        this.currRigidBodyOfAttacker.MovePosition(originalPosition);
     }
 
 
 
     public void fight()
     {
-        //goes back and forth
-        //success means D20 rolls at least as high as AC
-        //etc.
 
-        this.dude1GO()
-            {
-
-            D20Roll()
-
-            }
-        this.dude2GO()
-            {
-            D20Roll
-        }
-
-        public void damageHealthDude1()
-        {
-            if (this.dude2GO(D20Roll >= this.dude1 ac; dude1 hp -dude1 attack))
-        }
-    else wait(1.50f)
-    {
-    this.dude1GO()
+   // while (true)
     {
 
-        D20Roll()
+            this.attackerOriginalPosition = this.currentAttackerGO.transform.position; //need "transform.position" because it is a vector 3.
+            this.currRigidBodyOfAttacker = this.currentAttackerGO.GetComponent<Rigidbody>(); //rigidbody associated with whoever is currently attacking
+            this.attackMoveDistance *= -1;
 
-            }
-}
-
-    public void damageHealthDude2()
-        {
-            if (this.dude1GO(D20Roll >= this.dude2 ac; dude2 hp - dude1 attack))
-        }
-    else wait(1.50f)
-    {
-    this.dude2GO()
+            if(this.currentAttackerGO == this.dude1GO)
             {
-
-        D20Roll()
-
+                this.currentAttackerGO = this.dude2GO;
             }
-}
 
+            else
+            {
+                this.currentAttackerGO = this.dude1GO;
+            }
+
+            this.refereeInstance.StartCoroutine(MoveObjectRoutine()); // coroutines are non blocking code because they live within their own threads
     }
 
+
+    }
 
 
 }
--- Dungeon Crawler/Assets/Scripts/Referee Controller.cs
+++ Dungeon Crawler/Assets/Scripts/Referee Controller.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class RefereeController : MonoBehaviour
 {
-    public GameObject monsterGo;
-    public GameObject playerGo;
+    public GameObject monsterGo; //game object
+    public GameObject playerGo; //game object
     public TextMeshPro monsterSB;
-    public TextMeshPro playerSB;
+    public TextMeshPro playerSB; 
     private Monster theMonster;
+    private DeathMatch theMatch;
+    private Rigidbody currRigidBodyOfAttacker;
+    private float attackMoveDistance = 2.5f;
+    private Vector3 attackerOriginalPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -16,13 +20,25 @@ public class RefereeController : MonoBehaviour
         this.theMonster = new Monster("goblin");
         monsterSB.text = (this.theMonster.getData());
         this.playerSB.text = (MasterData.p.getData());
-       
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        this.theMatch = new DeathMatch(MasterData.p, this.theMonster, this.playerGo, this.monsterGo, this); 
+        MasterData.playerShouldAttack = true;
+        StartCoroutine(DelayBeforeFight);
         
     }
+
+    IEnumerator DelayBeforeFight()
+    {
+        yield return new WaitForSeconds(0.5f);
+        this.theMatch.fight();
+
+    }
+        //thread that acts like workerbee below!!!
+        //IEnumerator works like the run method in java
+
+   
+
+    }
+
+  
 }
+//start coroutine by giving it name of ienumerator
